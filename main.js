6bd48422eeef1a308d9bf5fc639222d00cfbaac0
HyeJun Lee <solar3070@naver.com>
2021-03-12 17:07:51 +0900

    function

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body) {
   return `
   <!doctype html>
   <html>
@@ -19,46 +19,59 @@ function templateHTML(title, list, body){
   `;
 }
 
-function templateList(filelist){
-  var list = '<ul>';
+function templateList(filelist) {
+  var list = "<ul>";
   var i = 0;
-  while(i < filelist.length) {
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i = i + 1;
+  while (i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
   }
-  list = list + '</ul>';
+  list = list + "</ul>";
   return list;
 }
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
 
-    if (pathname === '/') {//pathname이 루트라면
-        if (queryData.id === undefined) {
-            fs.readdir('./data', function(error, filelist){
-                var title = 'Welcome';
-                var description = 'Hello, Node.js';
-                var list = templateList(filelist);
-                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
-                response.writeHead(200);
-                response.end(template);
-            })
-        } else {
-            fs.readdir('./data', function(error, filelist){
-            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-                var title = queryData.id;
-                var list = templateList(filelist);
-                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
-                response.writeHead(200);
-                response.end(template);
-                });
-            });
-        }
+  if (pathname === "/") {
+    //pathname이 루트라면
+    if (queryData.id === undefined) {
+      fs.readdir("./data", function (error, filelist) {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var list = templateList(filelist);
+        var template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`
+        );
+        response.writeHead(200);
+        response.end(template);
+      });
     } else {
-        response.writeHead(404);
-        response.end('Not found');
+      fs.readdir("./data", function (error, filelist) {
+        fs.readFile(
+          `data/${queryData.id}`,
+          "utf8",
+          function (err, description) {
+            var title = queryData.id;
+            var list = templateList(filelist);
+            var template = templateHTML(
+              title,
+              list,
+              `<h2>${title}</h2>${description}`
+            );
+            response.writeHead(200);
+            response.end(template);
+          }
+        );
+      });
     }
+  } else {
+    response.writeHead(404);
+    response.end("Not found");
+  }
 });
 app.listen(3000);
