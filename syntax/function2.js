cd4dd2e0e8fc4afd93c3ab1e0cc8a35470734200
HyeJun Lee <solar3070@naver.com>
2021-03-12 17:06:06 +0900

    function

---
+++ syntax/function2.js
@@ -0,0 +1,8 @@
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.2)); //1
+
+function sum(first, second) { //prameter 매개변수
+    return first + second;
+}
+
+console.log(sum(2, 4)); //argument 인자
